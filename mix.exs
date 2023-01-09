9985d16c2f5331808631ae10dcd4e828163152f2
WanjikuMac <edwardscaroline32@gmail.com>
2023-01-09 10:08:43 +0300

    update project with latest phoenix and elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SbomPoc.MixProject do
     [
       app: :sbom_poc,
       version: "0.8.0",
-      elixir: "~> 1.11.2",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
