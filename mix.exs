a499e5c411020735edf039df7d44451e66b0ae2e
sigu <sigumagwa@gmail.com>
2025-01-20 15:57:40 +0000

    add xerml

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule SbomPoc.MixProject do
   def application do
     [
       mod: {SbomPoc.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :xmerl]
     ]
   end
 
